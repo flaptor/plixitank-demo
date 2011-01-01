bf25ffa2bf39cc0ab0e1c9d6acafe006a5a19c38
Diego Basch <dbasch@Diego-Baschs-Macbook.local>
2011-01-01 13:20:21 -0800

    added Gemfile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
 require 'indextank_client'
 
+
 class HomeController < ApplicationController
 
   def home
-      index = IndexTank::HerokuClient.new.get_index "YOUR INDEX NAME"
+      api_url = 'Your api url'
+      index_name = 'your index name'
+      index = IndexTank::ApiClient.new(api_url).get_index index_name
       @query = params[:query]
       print @query
       if @query and @query != ''
