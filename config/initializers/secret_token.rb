a3203ab0c3790c6db06bddbdda014ab3d09bd509
Diego Basch <dbasch@Diego-Baschs-Macbook.local>
2011-01-01 18:59:13 -0800

    added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlixitankDemo::Application.config.secret_token = 'a9a53b5a25108982543729329ed2fbb05564050d0603893d210af4f9b8f743f8f8e3ef11f2c5fe7ae780e1b38ae863c60554fb8a11d51c2245e7d0d4881fef7c'
